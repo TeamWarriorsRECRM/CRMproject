c17c4648e08fe392a90823c9e8b792e66f4c12e5
AMB <a.michael.boyle@gmail.com>
2021-03-01 12:57:28 -0500

    base server file setup

---
+++ server.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+
+const orm = require('../models/orm');
+const express = require('express');
+const apiRouter = require('./app/router/router')
+const app = express();
+
+const PORT = process.env.PORT || 8080
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.static('public'));
+
+
+
+app.post('/api/clientdb', async function(req, res){
+    const savedClients = await orm.getClients();
+    res.send(savedClients)
+})
+
+
+
+app.listen(PORT, function(){
+    console.log(`Database (name=${process.env.DB_NAME}); Serving app on: https://localhost:${PORT}`)
+})
