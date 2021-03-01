91943b1594417bb88fa8c889357fa30c8fdcdb3c
AMB <a.michael.boyle@gmail.com>
2021-03-01 14:53:29 -0500

    added server

--- server.js
+++ server.js
@@ -12,17 +12,6 @@ const PORT = process.env.PORT || 8080
 app.use(express.urlencoded({extended: true}));
 app.use(express.static('public'));
 
-// app.get('/', async function(req, res){
-//     res.sendFile(path.join(__dirname + '/index.html'))
-// });
-
-// app.post('/api/clientdb', async function(req, res){
-//     const savedClients = await orm.getClients();
-//     res.send(savedClients)
-// });
-
-
-
 app.listen(PORT, () => {
     console.log('Server is running', PORT)
 });
