1a2f8d307453ee1381ab770643e0c30ca22a1c22
AMB <a.michael.boyle@gmail.com>
2021-03-01 14:44:37 -0500

    server stuff

--- server.js
+++ server.js
@@ -1,24 +1,28 @@
 require('dotenv').config();
 
-const orm = require('../models/orm');
+const orm = require('./app/models/orm');
 const express = require('express');
-const apiRouter = require('./app/router/router')
+const apiRouter = require('./app/router/router');
+const { get } = require('http');
 const app = express();
+const path = require('path')
 
 const PORT = process.env.PORT || 8080
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.static('public'));
 
+// app.get('/', async function(req, res){
+//     res.sendFile(path.join(__dirname + '/index.html'))
+// });
+
+// app.post('/api/clientdb', async function(req, res){
+//     const savedClients = await orm.getClients();
+//     res.send(savedClients)
+// });
 
 
-app.post('/api/clientdb', async function(req, res){
-    const savedClients = await orm.getClients();
-    res.send(savedClients)
-})
 
-
-
-app.listen(PORT, function(){
-    console.log(`Database (name=${process.env.DB_NAME}); Serving app on: https://localhost:${PORT}`)
-})
+app.listen(PORT, () => {
+    console.log('Server is running', PORT)
+});
