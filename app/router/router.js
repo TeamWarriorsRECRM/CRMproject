57eeef34b4a8aa802bc6c043ad05feadcae762a8
AMB <a.michael.boyle@gmail.com>
2021-03-02 16:22:38 -0500

    dir rename

--- app/router/router.js
+++
@@ -1,8 +0,0 @@
-// const orm = require('../models/orm');
-
-// app.post('/api/clientdb', async function(req, res){
-//     const savedClients = await orm.getClients();
-//     res.send(savedClients)
-// })
-
-// module.exports = 
