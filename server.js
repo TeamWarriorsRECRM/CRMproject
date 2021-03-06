ebe293521d2e8a772995c7452f8c4b744fab9b12
Vithushan <vithushan@Vithushans-MacBook-Pro.local>
2021-03-06 18:40:36 -0500

    test

--- server.js
+++ server.js
@@ -24,94 +24,6 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
-<<<<<<< HEAD
-app.post("/api/index", passport.authenticate("local"), function (req, res) {
-  res.json(req.user);
-});
-
-app.post("/api/register", async function (req, res) {
-  let result = await db.User.create({
-    email: req.body.email,
-    password: req.body.password,
-  });
-  res.send(result);
-});
-
-app.get("/logout", function (req, res) {
-  req.logout();
-  console.log("redirecting server side");
-  res.redirect("/");
-});
-
-app.get("/api/database", function (req, res){
-  res.redirect('/database')
-  console.log("redirect to database")
-})
-
-app.get("/api/user_data", function (req, res) {
-  if (!req.user) {
-    res.json({});
-  } else {
-    res.json({
-      email: req.user.email,
-      id: req.user.id,
-    });
-  }
-});
-
-app.get("/api/database", async (req, res) => {
-  const list = await orm.getClients();
-  res.send(list);
-});
-
-app.post("/api/addClient", async (req, res) => {
-  let body = req.body;
-  orm.insertClient(body);
-  res.send();
-});
-
-app.put("/database.html/:firstname/:lastname/:email", async (req, res) => {
-  ////////////////////////////////////////////////////////////////////
-  const body = req.body;
-  const entry = await orm.updateInfo(body, body.id);
-  res.send(entry);
-});
-
-/////DELETES ENTRY
-app.delete("/database.html/:firstName/:lastName", (req, res) => {
-  orm.deleteClient(req.params.firstName, req.params.lastName);
-  res.send();
-});
-
-app.post("/database.html", (req, res) => {
-  let body = req.body;
-  orm.insertClient(body);
-  res.send();
-});
-
-app.post("/addClient.html", (req, res) => {
-  let body = req.body;
-  orm.insertClient(body);
-  res.send();
-});
-
-//////ONLY GETTING THE ID FOR THE SELECTED ENTRY
-app.get(`/database.html/:firstName/:lastName/:email`, async (req, res) => {
-  const [id] = await orm.getSingleClient(
-    req.params.firstName,
-    req.params.lastName,
-    req.params.email
-  );
-
-  const result = JSON.stringify(id.id);
-  res.send(result);
-});
-
-app.get("/api/clients", async (req, res) => {
-  const quickList = await orm.quickList();
-  res.send(quickList);
-});
-=======
 require("./app/routes/html-routes.js")(app);
 require("./app/routes/api-routes.js")(app);
 
@@ -201,7 +113,6 @@ require("./app/routes/api-routes.js")(app);
 //   console.log(quickList, "  FROM SERVER");
 //   res.send(quickList);
 // });
->>>>>>> 3c18bdabbb34153d876cff77f6c6a447a403ccd6
 
 db.sequelize.sync().then(function () {
   app.listen(PORT, function () {
